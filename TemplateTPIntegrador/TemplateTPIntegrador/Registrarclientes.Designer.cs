8b722e376afd9c5246c97501b96420df012c74fd
Valentin Rebo <79642482+valenrebo@users.noreply.github.com>
2024-10-24 12:56:14 -0300

    Creacion de modulo registrar clientes

---
+++ TemplateTPIntegrador/TemplateTPIntegrador/Registrarclientes.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace TemplateTPIntegrador
+{
+    partial class Registrarclientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Registrarusuarioboton = new System.Windows.Forms.Button();
+            this.eticliente = new System.Windows.Forms.Label();
+            this.etiDireccion = new System.Windows.Forms.Label();
+            this.Etitelefono = new System.Windows.Forms.Label();
+            this.etiEmail = new System.Windows.Forms.Label();
+            this.EtiActivo = new System.Windows.Forms.Label();
+            this.activosiono = new System.Windows.Forms.CheckBox();
+            this.textnombrecliente = new System.Windows.Forms.TextBox();
+            this.texdireccioncliente = new System.Windows.Forms.TextBox();
+            this.textelcliente = new System.Windows.Forms.TextBox();
+            this.texemailcliente = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.etiapellido = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.texDNI = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Registrarusuarioboton
+            // 
+            this.Registrarusuarioboton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Registrarusuarioboton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registrarusuarioboton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Registrarusuarioboton.Location = new System.Drawing.Point(746, 477);
+            this.Registrarusuarioboton.Name = "Registrarusuarioboton";
+            this.Registrarusuarioboton.Size = new System.Drawing.Size(160, 73);
+            this.Registrarusuarioboton.TabIndex = 9;
+            this.Registrarusuarioboton.Text = "Registrar Usuario";
+            this.Registrarusuarioboton.UseVisualStyleBackColor = false;
+            // 
+            // eticliente
+            // 
+            this.eticliente.AutoSize = true;
+            this.eticliente.Location = new System.Drawing.Point(77, 148);
+            this.eticliente.Name = "eticliente";
+            this.eticliente.Size = new System.Drawing.Size(65, 20);
+            this.eticliente.TabIndex = 10;
+            this.eticliente.Text = "Nombre";
+            // 
+            // etiDireccion
+            // 
+            this.etiDireccion.AutoSize = true;
+            this.etiDireccion.Location = new System.Drawing.Point(77, 335);
+            this.etiDireccion.Name = "etiDireccion";
+            this.etiDireccion.Size = new System.Drawing.Size(75, 20);
+            this.etiDireccion.TabIndex = 11;
+            this.etiDireccion.Text = "Direccion";
+            // 
+            // Etitelefono
+            // 
+            this.Etitelefono.AutoSize = true;
+            this.Etitelefono.Location = new System.Drawing.Point(77, 386);
+            this.Etitelefono.Name = "Etitelefono";
+            this.Etitelefono.Size = new System.Drawing.Size(71, 20);
+            this.Etitelefono.TabIndex = 12;
+            this.Etitelefono.Text = "Telefono";
+            // 
+            // etiEmail
+            // 
+            this.etiEmail.AutoSize = true;
+            this.etiEmail.Location = new System.Drawing.Point(77, 433);
+            this.etiEmail.Name = "etiEmail";
+            this.etiEmail.Size = new System.Drawing.Size(48, 20);
+            this.etiEmail.TabIndex = 13;
+            this.etiEmail.Text = "Email";
+            // 
+            // EtiActivo
+            // 
+            this.EtiActivo.AutoSize = true;
+            this.EtiActivo.Location = new System.Drawing.Point(77, 480);
+            this.EtiActivo.Name = "EtiActivo";
+            this.EtiActivo.Size = new System.Drawing.Size(52, 20);
+            this.EtiActivo.TabIndex = 14;
+            this.EtiActivo.Text = "Activo";
+            // 
+            // activosiono
+            // 
+            this.activosiono.AutoSize = true;
+            this.activosiono.Location = new System.Drawing.Point(280, 479);
+            this.activosiono.Name = "activosiono";
+            this.activosiono.Size = new System.Drawing.Size(22, 21);
+            this.activosiono.TabIndex = 15;
+            this.activosiono.UseVisualStyleBackColor = true;
+            // 
+            // textnombrecliente
+            // 
+            this.textnombrecliente.Location = new System.Drawing.Point(280, 142);
+            this.textnombrecliente.Name = "textnombrecliente";
+            this.textnombrecliente.Size = new System.Drawing.Size(242, 26);
+            this.textnombrecliente.TabIndex = 16;
+            // 
+            // texdireccioncliente
+            // 
+            this.texdireccioncliente.Location = new System.Drawing.Point(280, 335);
+            this.texdireccioncliente.Name = "texdireccioncliente";
+            this.texdireccioncliente.Size = new System.Drawing.Size(242, 26);
+            this.texdireccioncliente.TabIndex = 17;
+            // 
+            // textelcliente
+            // 
+            this.textelcliente.Location = new System.Drawing.Point(280, 389);
+            this.textelcliente.Name = "textelcliente";
+            this.textelcliente.Size = new System.Drawing.Size(242, 26);
+            this.textelcliente.TabIndex = 18;
+            // 
+            // texemailcliente
+            // 
+            this.texemailcliente.Location = new System.Drawing.Point(280, 436);
+            this.texemailcliente.Name = "texemailcliente";
+            this.texemailcliente.Size = new System.Drawing.Size(242, 26);
+            this.texemailcliente.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 26);
+            this.textBox1.TabIndex = 21;
+            // 
+            // etiapellido
+            // 
+            this.etiapellido.AutoSize = true;
+            this.etiapellido.Location = new System.Drawing.Point(77, 192);
+            this.etiapellido.Name = "etiapellido";
+            this.etiapellido.Size = new System.Drawing.Size(65, 20);
+            this.etiapellido.TabIndex = 20;
+            this.etiapellido.Text = "Apellido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 30);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Fecha de nacimiento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 30);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "DNI";
+            // 
+            // texDNI
+            // 
+            this.texDNI.Location = new System.Drawing.Point(280, 288);
+            this.texDNI.Name = "texDNI";
+            this.texDNI.Size = new System.Drawing.Size(242, 26);
+            this.texDNI.TabIndex = 24;
+            // 
+            // Registrarclientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 639);
+            this.Controls.Add(this.texDNI);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.etiapellido);
+            this.Controls.Add(this.texemailcliente);
+            this.Controls.Add(this.textelcliente);
+            this.Controls.Add(this.texdireccioncliente);
+            this.Controls.Add(this.textnombrecliente);
+            this.Controls.Add(this.activosiono);
+            this.Controls.Add(this.EtiActivo);
+            this.Controls.Add(this.etiEmail);
+            this.Controls.Add(this.Etitelefono);
+            this.Controls.Add(this.etiDireccion);
+            this.Controls.Add(this.eticliente);
+            this.Controls.Add(this.Registrarusuarioboton);
+            this.Name = "Registrarclientes";
+            this.Text = "Registrar clientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Registrarusuarioboton;
+        private System.Windows.Forms.Label eticliente;
+        private System.Windows.Forms.Label etiDireccion;
+        private System.Windows.Forms.Label Etitelefono;
+        private System.Windows.Forms.Label etiEmail;
+        private System.Windows.Forms.Label EtiActivo;
+        private System.Windows.Forms.CheckBox activosiono;
+        private System.Windows.Forms.TextBox textnombrecliente;
+        private System.Windows.Forms.TextBox texdireccioncliente;
+        private System.Windows.Forms.TextBox textelcliente;
+        private System.Windows.Forms.TextBox texemailcliente;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label etiapellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox texDNI;
+    }
+}
